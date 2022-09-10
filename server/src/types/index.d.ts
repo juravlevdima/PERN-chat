cc6987a9ff12afb34c91b7c09f8d9a201076bc2b
juravlevdima <juravlevdima96@gmail.com>
2022-09-10 22:57:09 +0600

    upd

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,11 +1,1 @@
-// import { IUserPublicData } from './user.types'
-//
-// declare global {
-//   namespace Express {
-//     export interface Request {
-//       user?: IUserPublicData
-//     }
-//   }
-// }
-//
 // export {}
