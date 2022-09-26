a0e3745d011e176a414c02dd41456996e830e7db
juravlevdima <juravlevdima96@gmail.com>
2022-09-26 19:14:09 +0600

    setup for heroku 14

--- server/src/services/postgres.ts
+++ server/src/services/postgres.ts
@@ -3,18 +3,21 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const sequelize = new Sequelize(
-  // @ts-ignore
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    logging: false
-  }
-)
+// export const sequelize = new Sequelize(
+//   // @ts-ignore
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     logging: false
+//   }
+// )
+
+// @ts-ignore
+export const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 const dbConnect = async (): Promise<void> => {
   try {
