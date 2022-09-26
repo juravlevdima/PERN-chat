1663be56f516004b88003c151820b871fdb9384d
juravlevdima <juravlevdima96@gmail.com>
2022-09-26 19:36:21 +0600

    setup for heroku 15

--- server/src/services/postgres.ts
+++ server/src/services/postgres.ts
@@ -17,7 +17,7 @@ dotenv.config()
 // )
 
 // @ts-ignore
-export const sequelize = new Sequelize(process.env.DATABASE_URL)
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {"ssl": true})
 
 const dbConnect = async (): Promise<void> => {
   try {
