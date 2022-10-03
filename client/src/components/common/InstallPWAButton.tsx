c419155436380beb195576744964fe7754b7a9af
juravlevdima <juravlevdima96@gmail.com>
2022-10-03 16:21:29 +0600

    add install button

---
+++ client/src/components/common/InstallPWAButton.tsx
@@ -0,0 +1,46 @@
+import { FC, useEffect, useState, MouseEvent } from 'react'
+import { BeforeInstallPromptEvent } from '../../types/events.types'
+import installIcon from '../../images/icons/install.svg'
+
+const InstallPwaButton: FC = () => {
+  const [supportsPWA, setSupportsPWA] = useState(false)
+  const [promptInstall, setPromptInstall] = useState<BeforeInstallPromptEvent | null>(null)
+
+  useEffect(() => {
+    const handler = (e: BeforeInstallPromptEvent) => {
+      e.preventDefault()
+      setSupportsPWA(true)
+      setPromptInstall(e)
+    }
+    window.addEventListener('beforeinstallprompt', handler)
+
+    return () => window.removeEventListener('transitionend', handler)
+  }, [])
+
+  const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    if (!promptInstall) {
+      return
+    }
+
+    promptInstall?.prompt()
+  }
+
+  if (!supportsPWA) {
+    return null
+  }
+
+  return (
+    <button
+      className="w-6 mr-3"
+      id="setup_button"
+      aria-label="Install app"
+      title="Install app"
+      onClick={onClick}
+    >
+      <img src={installIcon} alt="Install"/>
+    </button>
+  )
+}
+
+export default InstallPwaButton
